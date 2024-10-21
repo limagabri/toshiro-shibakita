4662a71457d9806dbe826b152cae55b2f80115c0
Gabriel Lima <gabriel.souza.lima10@hotmail.com>
2024-10-21 08:22:42 -0300

    Update index.php
    suporte a variáveis de ambiente para as credenciais do banco de dados.

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <html>
-
 <head>
-<title>Exemplo PHP</title>
+    <title>Exemplo PHP</title>
 </head>
 <body>
 
@@ -9,40 +8,34 @@
 ini_set("display_errors", 1);
 header('Content-Type: text/html; charset=iso-8859-1');
 
+echo 'Versão Atual do PHP: ' . phpversion() . '<br>';
 
-
-echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
-
-$servername = "54.234.153.24";
-$username = "root";
-$password = "Senha123";
-$database = "meubanco";
+// Utilizando variáveis de ambiente
+$servername = getenv('DB_HOST') ?: 'localhost';
+$username = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASS') ?: '';
+$database = getenv('DB_NAME') ?: 'meubanco';
 
 // Criar conexão
-
-
 $link = new mysqli($servername, $username, $password, $database);
 
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+if ($link->connect_error) {
+    die("Falha na conexão: " . $link->connect_error);
 }
 
-$valor_rand1 =  rand(1, 999);
+$valor_rand1 = rand(1, 999);
 $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
-
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
-
+$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) 
+          VALUES ('$valor_rand1', '$valor_rand2', '$valor_rand2', 
+                  '$valor_rand2', '$valor_rand2', '$host_name')";
 
 if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
+    echo "Novo registro criado com sucesso!";
 } else {
-  echo "Error: " . $link->error;
+    echo "Erro: " . $link->error;
 }
-
 ?>
 </body>
 </html>
